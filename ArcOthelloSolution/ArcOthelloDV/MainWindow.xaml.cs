f556edd89a20d437ab025f4448fceb0e1c84d8d3
Célien Donzé <celien.donze@he-arc.ch>
2018-12-21 13:28:56 +0100

    added numbers and letters around grid

--- ArcOthelloSolution/ArcOthelloDV/MainWindow.xaml.cs
+++ ArcOthelloSolution/ArcOthelloDV/MainWindow.xaml.cs
@@ -23,19 +23,48 @@ namespace ArcOthelloDV
         public MainWindow()
         {
             InitializeComponent();
-            for(int i = 0 ; i < 7 ; i++)
+            for (int i = 0 ; i < 9 ; i++)
             {
-                for(int j = 0 ; j < 9 ; j++)
+                for (int j = 0 ; j < 11 ; j++)
                 {
-
-                    Rectangle rect = new Rectangle();
-                    rect.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0));
-                    Grid.SetRow(rect, i);
-                    Grid.SetColumn(rect, j);
-
-                    board.Children.Add(rect);
+                    if (i > 0 && i < 8 && j > 0 && j < 10)
+                    {
+                        Rectangle rect = new Rectangle();
+                        rect.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                        rect.Fill = new SolidColorBrush(Color.FromRgb(0, 178, 0));
+                        Grid.SetRow(rect, i);
+                        Grid.SetColumn(rect, j);
+                        board.Children.Add(rect);
+                    }
+                    else
+                    {
+                        if (i == 0 && j > 0 && j < 10)
+                        {
+                            TextBlock tb = new TextBlock();
+                            tb.Inlines.Add(((char)(j+64)).ToString());
+                            tb.TextAlignment = TextAlignment.Center;
+                            tb.VerticalAlignment = VerticalAlignment.Center;
+                            tb.FontSize = 22;
+                            tb.FontWeight = FontWeights.Bold;
+                            Grid.SetRow(tb, i);
+                            Grid.SetColumn(tb, j);
+                            board.Children.Add(tb);
+                        }
+                        else if (j == 0 && i > 0 && i <8)
+                        {
+                            TextBlock tb = new TextBlock();
+                            tb.Inlines.Add((i).ToString());
+                            tb.TextAlignment = TextAlignment.Center;
+                            tb.VerticalAlignment = VerticalAlignment.Center;
+                            tb.FontSize = 22;
+                            tb.FontWeight = FontWeights.Bold;
+                            Grid.SetRow(tb, i);
+                            Grid.SetColumn(tb, j);
+                            board.Children.Add(tb);
+                        }
+                    }
                 }
             }
-        }
+        }   
     }
 }
